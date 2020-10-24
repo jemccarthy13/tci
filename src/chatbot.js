b2d8723865c87367d07d76a8bcf0b291eac7f5d6
jmccarthy <jmccarthy@shoc.us>
2020-10-23 22:13:42 -0500

    credentials and settings retrieved from input fields

--- src/chatbot.js
+++ src/chatbot.js
@@ -25,27 +25,20 @@ export default class Chatbot extends React.Component {
       if (stanza.is('message')) {
         if (stanza.attrs.from.indexOf('chatbot')===-1){
           if (stanza.children[2]){
-            //console.log("received message:" , stanza.children[2].attrs.stamp)
+            console.log("received message:" , stanza.children[2].attrs.stamp)
           }
-          //console.log(stanza)
+          console.log(stanza)
         }
       }
     })
 
     xmpp.on('online', async address =>{
       this.setOnline()      
-      let onlinemsgs = []
-      let msg={
-        time:"",
-        window:"",
-        callsign:"system",
-        text:"Connected to XMPP server as " + this.xmpp.jid.local
-      }
-      onlinemsgs.push(msg)
-      this.setState({sentMessages: this.state.sentMessages.concat(onlinemsgs)})
+      this.systemMsg("Connected to XMPP server as " + this.xmpp.jid.local)
+      snackbar.alert("Connected to XMPP server as " + this.xmpp.jid.local, 5000, "green")
     })
 
-    xmpp.start().catch(console.error)
+    xmpp.start().catch(this.addErrorMsg)
     return xmpp;
   }
 
@@ -63,7 +56,41 @@ export default class Chatbot extends React.Component {
       roomsOnline:[],
       messages:[],
       initMessages:[],
+      conference:this.CONFERENCE,
+      domain:this.DOMAIN,
+      username:this.USERNAME,
+      password:this.PASSWORD,
+      service:this.SERVICE,
     }
+  }
+  
+  btnStyle = {
+    margin: "5px",
+    width:"25%",
+    textAlign:"center"
+  };
+
+  textAreaStyle = {
+    width:"90%",
+    height:"400px",
+    margin:"10px",
+  }
+
+  addErrorMsg=()=>{
+    this.systemMsg("Error connecing to XMPP server. Please check settings and credentials.")
+    snackbar.alert("Error connecing to XMPP server. Please check settings and credentials.", 10000, "red")
+  }
+
+  systemMsg(myText){
+    let onlinemsgs = []
+      let msg={
+        time:"",
+        window:"",
+        callsign:"system",
+        text: myText
+      }
+      onlinemsgs.push(msg)
+      this.setState({sentMessages: this.state.sentMessages.concat(onlinemsgs)})
   }
 
   checkMessages = (startTime) => {
@@ -95,7 +122,7 @@ export default class Chatbot extends React.Component {
 
   async setPresence(room, id=this.xmpp.jid.local){
     await this.xmpp.send(xml('presence', 
-    {to: room+'@' +process.env.REACT_APP_XMPP_CONFERENCE+'/'+id}))
+    {to: room+'@' +this.state.conference+'/'+id}))
   }
 
   setPresenceInRooms = async (id=this.xmpp.jid.local) => {
@@ -117,14 +144,14 @@ export default class Chatbot extends React.Component {
   async sendMessage(message){
     const msg = xml(
       'message',
-      {type: 'groupchat', to: message.window+"@"+this.CONFERENCE},
+      {type: 'groupchat', to: message.window+"@"+this.state.conference},
       xml('body', {}, "<"+message.callsign+"> "+message.text)
     )
     await this.xmpp.send(msg)
   }
 
   connect = () =>{
-    this.xmpp = this.newXmpp(this.SERVICE, this.DOMAIN, this.USERNAME, this.PASSWORD)
+    this.xmpp = this.newXmpp(this.state.service, this.state.domain, this.state.username, this.state.password)
   }
 
   play = async () => {
@@ -165,16 +192,31 @@ export default class Chatbot extends React.Component {
     snackbar.alert("Injector reset", 3000, "yellow")
   }
 
-  btnStyle = {
-    margin: "5px",
-    width:"25%",
-    textAlign:"center"
-  };
-
-  textAreaStyle = {
-    width:"90%",
-    height:"400px",
-    margin:"10px",
+  handleInputChange = (val) =>{
+    return(event)=>{
+      switch(val){
+        case "domain":
+          this.setState({domain:event.target.value})
+          break;
+        case "service":
+          this.setState({service:event.target.value})
+          break;
+        case "username":
+          this.setState({username:event.target.value})
+          break;
+        case "password":
+          this.setState({password:event.target.value})
+          break;
+        case "simtime":
+          this.setState({simtime:event.target.value})
+          break;
+        case "conference":
+          this.setState({conference:event.target.value})
+          break;
+        default:
+          break;
+      }
+    }
   }
 
   render(){
@@ -189,12 +231,12 @@ export default class Chatbot extends React.Component {
     })
     return (<div style={{width:"100%"}}>
         <div>Settings:
-          <div><label>Sim Time: </label><input defaultValue="hh:mm:ss"/></div>
-          <div><label>XMPP Service: </label><input defaultValue={this.SERVICE}/></div>
-          <div><label>Domain: </label><input defaultValue={this.DOMAIN}/></div>
-          <div><label>Conference: </label><input defaultValue={this.CONFERENCE}/></div>
-          <div><label>Usename: </label><input defaultValue={this.USERNAME}/></div>
-          <div><label>Password: </label><input defaultValue={this.PASSWORD}/></div>
+          <div><label>Sim Time: </label><input defaultValue="hh:mm:ss" onChange={this.handleInputChange("simtime")}/></div>
+          <div><label>XMPP Service: </label><input defaultValue={this.SERVICE} onChange={this.handleInputChange("service")}/></div>
+          <div><label>Domain: </label><input defaultValue={this.DOMAIN} onChange={this.handleInputChange("domain")}/></div>
+          <div><label>Conference: </label><input defaultValue={this.CONFERENCE} onChange={this.handleInputChange("conference")}/></div>
+          <div><label>Usename: </label><input defaultValue={this.USERNAME} onChange={this.handleInputChange("username")}/></div>
+          <div><label>Password: </label><input defaultValue={this.PASSWORD} onChange={this.handleInputChange("password")}/></div>
           <button onClick={this.connect}>Connect</button>
         </div>
         <div style={{width:"100%", textAlign:"center", alignItems:"center"}}>
